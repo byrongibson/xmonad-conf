6a3fb8f07f8d3b58c1799ea67dcd2fd7dffaccd5
Your Name <you@example.com>
2014-04-15 14:32:58 -0700

    update

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,10 +2,13 @@
 -- https://gist.github.com/kofno/1300108
  
 import XMonad
+import XMonad.Util.Run
 import XMonad.Config.Gnome
+import XMonad.Hooks.ICCCMFocus
 --import XMonad.Config.Desktop
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
+--import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Layout.Gaps
 import Data.List
  
@@ -13,20 +16,23 @@ myManageHook = composeAll . concat $
   [ [ className =? "Firefox-bin" --> doShift "6:ff" ]
   , [ className =? "Firefox" --> doShift "6:ff" ]
   , [(className =? "Firefox" <&&> resource =? "Dialog") --> doFloat]
-  , [ className =? "Emacs" --> doShift "2:emacs" ]
-  , [ className =? "Emacsclient" --> doShift "2:emacs" ]
+  , [ className =? "Vim" --> doShift "2:vim" ]
+  , [ className =? "VimClient" --> doShift "2:vim" ]
   , [ className =? "Unity-2d-panel" --> doIgnore ]
   , [ className =? "Unity-2d-launcher" --> doIgnore ]
   ]
  
-myLayouts = gaps [(U, 24)] $ layoutHook gnomeConfig
+--myLayouts = gaps [(U, 24)] $ layoutHook gnomeConfig
+myLayouts = avoidStruts $ layoutHook gnomeConfig
  
 main = xmonad $ gnomeConfig
   { workspaces = ["1","2","3","4","5","6","7","8","9","0","-","="]
-  , manageHook = myManageHook <+> manageHook gnomeConfig
+  --, manageHook = myManageHook <+> manageHook gnomeConfig
+  , manageHook = manageDocks <+> myManageHook <+> manageHook gnomeConfig 
+  , layoutHook = myLayouts
+  , logHook = takeTopFocus
   -- mod1Mask = Left Alt, mod3Mask = Right Alt, mod4Mask = Windows Key
   , modMask = mod1Mask
-  , layoutHook = myLayouts
   , terminal = "urxvt"
   , focusFollowsMouse = False
   }
